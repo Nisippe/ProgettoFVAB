51384cb7f06a41d93e24f0ce6e1a2916223828ad
antonella roscigno <you@example.com>
2023-05-06 13:09:05 +0200

    Statistica 0.2

--- StatisticaVera.py
+++ StatisticaVera.py
@@ -2,23 +2,79 @@ import pandas as pd
 
 rows=pd.read_csv('TXT/MediaRighe.txt')
 cols=pd.read_csv('TXT/MediaColonne.txt')
+train_validation=pd.read_csv('CSV/Responses_train+validation.csv')
+test=train_validation.apply(pd.to_numeric,errors='coerce')
+test=test.drop('Gait ID', axis=1)
 
 medieRighe=rows['mediaRighe']
 medieColonne=cols['mediaColonne']
+col1=cols['num1']
+col2=cols['num2']
+col3=cols['num3']
+col4=cols['num4']
 col5=cols['num5']
+
+def check_big_col(col1,col2,col3,col4,col5):
+    list=[col1,col2,col3,col4,col5]
+    return list.index(max(list))
 
 val = 0
 for i in range(60):
     df = medieColonne[val:val + 4]
     listdf = df.tolist()
-    max(listdf)
-    index = listdf.index(max(listdf))
+    maxMedia=max(listdf)
+    index = listdf.index(maxMedia)
     print(index)
-    colNeutra = medieColonne[val + 4]
-
+    index_n=check_big_col(col1,col2, col3, col4, col5)
     for j in range(87):
         partecipante=medieRighe[j]
+        if (df[0].isnull().values.any()):
+            if(index==3):
+                if(index_n==4):
+                    test=test[j,val]=3
+                    test=test[j,val+1]=3
+                    test=test[j,val+2]=3
+                    test=test[j,val+3]=5
+                    '''
+                    Soluzione neutra completa(:
+                    3-3-3-5
+                    '''
+                elif(index_n==3):
+
+                    '''
+                    Soluzione neutra:
+                    Media partecipanti con neutro 4
+                    ?-?-?-4
+                    '''
+                else:
+                    '''
+                    Soluzione strana:
+                    Media partecipanti con neutro 3 o minore
+                    ?-?-?-3
+                    '''
+            else:
+                for i in range(0,4):
+                    if index_n==5:
+                        if i is index:
+                            test[val+i]=5
+                        else:
+                            test[val+i]=1
+                    elif index_n==4:
+                        if i is index:
+                            test[val+i]=4
+                        else:
+                            #sticazzi
+                    elif index_n==3:
+                        #mammt
 
 
+                '''
+                Soluzione prime 3 colonne:
+                -Se max value = 5 allora:
+                gli altri vanno a 1
+                5-1-1-1
+                1-5-1-1
+                1-1-5-1
+                '''
 
     val+=4
