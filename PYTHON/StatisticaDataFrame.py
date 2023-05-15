c7284159d1289e2b42e7c4271826e241fa1c96ce
Nisippe <g.nisivoccia@studenti.unisa.it>
2023-05-16 00:57:59 +0200

    Aggiustato etichette_percentuali + Funzioni + Mediapipe 0.1 + Domande + Creato file Regression, cartella python + Creati nuovi video temporanei

---
+++ PYTHON/StatisticaDataFrame.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+rows=pd.read_csv('../TXT/MediaRighe.txt')
+cols=pd.read_csv('../TXT/MediaColonne.txt')
+train_validation=pd.read_csv('../CSV/Responses_train+validation.csv')
+test=train_validation.apply(pd.to_numeric,errors='coerce')
+test=test.drop('Gait ID', axis=1)
+
+medieRighe=rows['mediaRighe']
+medieColonne=cols['mediaColonne']
+col1=cols['num1']
+col2=cols['num2']
+col3=cols['num3']
+col4=cols['num4']
+col5=cols['num5']
+
+etichette_perc=pd.read_csv('../TXT/Etichette_Percentuali.txt')
+
+
+def check_big_col(col1,col2,col3,col4,col5):
+    list=[col1,col2,col3,col4,col5]
+    return list.index(max(list))
+
+val = 0
+for i in range(61):
+    n=0
+    list_perc = []
+    list=[]
+    for j in range(0,4):
+        list_perc.append(round(medieColonne[val+j],2))
+        n+=list_perc[j]
+    list.append('Vid'+str(i))
+    for j in range(0,4):
+        list.append(round(float((list_perc[j]/5)*100),1))
+    etichette_perc.loc[i]=list
+    etichette_perc.to_csv('../TXT/Etichette_Percentuali.txt',sep=',')
+    val+=4
---
+++ PYTHON/StatisticaMedie.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+train_validation=pd.read_csv('../CSV/Responses_train+validation.csv')
+test=train_validation.apply(pd.to_numeric,errors='coerce')
+test=test.drop('Gait ID', axis=1)
+
+columns=test.columns
+list1 = []
+list2 = []
+list3 = []
+list4 = []
+list5 = []
+def count_n_video_column(df,col,value):
+    return (df[col]==value).sum()
+
+
+for col in columns:
+    count1 = count_n_video_column(test, col, 1)
+    count2 = count_n_video_column(test, col, 2)
+    count3 = count_n_video_column(test, col, 3)
+    count4 = count_n_video_column(test, col, 4)
+    count5 = count_n_video_column(test,col,5)
+    list1.append(count1)
+    list2.append(count2)
+    list3.append(count3)
+    list4.append(count4)
+    list5.append(count5)
+
+#Soluzione Medie Colonne
+df2 = test.mean(axis = 0, skipna = True)
+df2=pd.DataFrame(df2)
+
+
+df2=df2.assign(num1=list1,num2=list2,num3=list3,num4=list4,num5=list5)
+df2.to_csv('TXT/MediaColonne.txt',sep=',')
+
+
+
+
+
+
+
+
+
