b6105ab7544600cd5c53a5ebcd138d28dea8fc32
Nisippe <g.nisivoccia@studenti.unisa.it>
2023-06-15 22:22:51 +0200

    mi ero scordato di pushare

--- PYTHON/Regression.py
+++ PYTHON/Regression.py
@@ -1,7 +1,7 @@
 import keras
 from keras import Sequential, regularizers
 from keras.layers import Dense, LSTM, Conv1D
-
+import matplotlib.pyplot as plt
 import Feature_Extractor as fe
 import Funzioni as f
 import numpy as np
@@ -24,16 +24,21 @@ y_train=np.array(y_train)
 print(X_train.shape)
 print(y_train.shape)
 model = Sequential()
-model.add(Dense(64, activation="relu",input_shape=(24,25088)))
+inputs = keras.layers.Input(shape=(24,25088))
+lstm_out = keras.layers.LSTM(512)(inputs)
+dense_out=keras.layers.Dense(256)(lstm_out)
+dense2_out=keras.layers.Dense(128)(dense_out)
+dense3_out=keras.layers.Dense(64)(dense2_out)
+dense4_out=keras.layers.Dense(32)(dense3_out)
+outputs = keras.layers.Dense(1)(dense4_out)
 
-model.add(Dense(32, activation="relu",kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
-    bias_regularizer=regularizers.L2(1e-4),
-    activity_regularizer=regularizers.L2(1e-5)))
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer='adam',loss='mae',metrics=['mse'])
+model.summary()
 
-model.add(Dense(1,activation="relu",kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
-    bias_regularizer=regularizers.L2(1e-4),
-    activity_regularizer=regularizers.L2(1e-5)))
-
-model.compile(optimizer='adadelta', metrics=['mse','mae'])
-model.fit(X_train,y_train,epochs=100,batch_size=128,validation_data=(X_train,y_train))
+history=model.fit(X_train, y_train, epochs=100, validation_data=(X_train,y_train))
+plt.plot(history.history["loss"], label="Training Loss")
+plt.plot(history.history["val_loss"], label="Validation Loss")
+plt.legend()
+plt.show()
 model.save('Modello.keras')
